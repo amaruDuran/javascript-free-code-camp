0c4d4ddc5cef3b7263d49611b3eb407b3f850f91
amaruDuran <amaruduran@gmail.com>
2023-07-21 15:52:50 -0300

    OK rot13

--- rot13.js
+++ rot13.js
@@ -3,20 +3,27 @@ function rot13(str) {
     var alfabetoList = alfabeto.split("");
     var resul = "";
     let strList = str.split("");
+
     for (const caracter of strList) {
         let lenAlfabeto = alfabeto.length
         let indiceCaracterBuscado = alfabetoList.indexOf(caracter);
-        if (indiceCaracterBuscado > -1 && 
-            (lenAlfabeto - 1 - indiceCaracterBuscado) >= 13) {
-            let indiceRot = indiceCaracterBuscado + 13;
-            resul += alfabetoList[indiceRot];
+        let indiceRot = indiceCaracterBuscado + 13;
+
+        if (indiceCaracterBuscado > -1 
+            && (lenAlfabeto - 1 - indiceCaracterBuscado) >= 13) {
+                resul += alfabetoList[indiceRot];
+        }
+        else if (indiceCaracterBuscado > -1 
+            && (lenAlfabeto - 1 - indiceCaracterBuscado) < 13) {
+                let alfabetoCircularList = (alfabeto + alfabeto).split("");
+                resul += alfabetoCircularList[indiceRot];
         }
         else{
-            console.log(caracter);
             resul += caracter;
         }
     }
     return resul;
   }
   
+  // retorna: FREE CODE CAMP
   console.log(rot13("SERR PBQR PNZC"));
