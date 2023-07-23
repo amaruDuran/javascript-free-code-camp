1384dd69f65dac8db42cf929e2f3c5a01fa3928d
amaruDuran <amaruduran@gmail.com>
2023-07-23 18:33:28 -0300

    test regexx with 10 numbers done

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -13,10 +13,16 @@ function telephoneRegexWithParenthesisAndSpace(str) {
     return telRegexWithParenthesisAndSpace.test(str);
 }
 
+function telephoneRegexWith10Numbers(str){
+    let telephoneRegexWith10Numbers = /\d{10}/g
+    return telephoneRegexWith10Numbers.test(str);
+}
+
 function telephoneCheck(str) {
     return (telephoneRegexWithMinus(str)
      || telephoneRegexWithParenthesis(str)
-     || telephoneRegexWithParenthesisAndSpace(str));
+     || telephoneRegexWithParenthesisAndSpace(str)
+     || telephoneRegexWith10Numbers(str));
   }
 
 console.log("TEST TelephoneRegexWithMinus") 
@@ -44,3 +50,12 @@ console.log(telephoneRegexWithParenthesisAndSpace("(555) 555-5555"));
 console.log(telephoneRegexWithParenthesisAndSpace("555 555 5555"));
 console.log(telephoneRegexWithParenthesisAndSpace("5555555555"));
 console.log(telephoneRegexWithParenthesisAndSpace("1 555 555 5555"));
+
+console.log("")
+console.log("TEST telephoneRegexWith10Numbers")
+console.log(telephoneRegexWith10Numbers("555-555-5555"));
+console.log(telephoneRegexWith10Numbers("(555)555-5555"));
+console.log(telephoneRegexWith10Numbers("(555) 555-5555"));
+console.log(telephoneRegexWith10Numbers("555 555 5555"));
+console.log(telephoneRegexWith10Numbers("5555555555"));
+console.log(telephoneRegexWith10Numbers("1 555 555 5555"));
