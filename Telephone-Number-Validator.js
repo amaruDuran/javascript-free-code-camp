b4f9ed407c0d227c28f0bad475c5c73c4f46e338
amaruDuran <amaruduran@gmail.com>
2023-07-23 18:05:01 -0300

    test regex with minus OK

--- Telephone-Number-Validator.js
+++ Telephone-Number-Validator.js
@@ -1,12 +1,24 @@
+function telephoneRegexWithMinus(str){
+    let telRegexWithMinus = /(\d{3})-(\d{3})-(\d{4})/g
+    return telRegexWithMinus.test(str);
+}
+
+function telephoneRegexWithParenthesis(str){
+    /[\(]?(\d{3})[\)-\s]?\s?(\d{3})[-\s]?(\d{4})/g
+}
+
 function telephoneCheck(str) {
-    // let telRegex = /(\d{3,4})-?/g
-    let telRegex = /[\(]?(\d{3})[\)-\s]?\s?(\d{3})[-\s]?(\d{4})/g
+    let testTelRegexWithMinus = telephoneRegexWithMinus(str);
+    let telRegexWithParenthesis = 
+    let telRegexWithParenthesisAndSpace = /(\(\d{3}\))\s(\d{3})-(\d{4})/g
+    telRege3 = /(\(\d{3}\))|(\d{3})[-\s]?\s?(\d{3})[-\s]?(\d{4})/g
     return telRegex.test(str);
   }
 
-console.log(telephoneCheck("555-555-5555"));
-console.log(telephoneCheck("(555)555-5555"));
-console.log(telephoneCheck("(555) 555-5555"));
-console.log(telephoneCheck("555 555 5555"));
-console.log(telephoneCheck("5555555555"));
-console.log(telephoneCheck("1 555 555 5555"));
+// TEST TelephoneRegexWithMinus.
+console.log(telephoneRegexWithMinus("555-555-5555"));
+console.log(telephoneRegexWithMinus("(555)555-5555"));
+console.log(telephoneRegexWithMinus("(555) 555-5555"));
+console.log(telephoneRegexWithMinus("555 555 5555"));
+console.log(telephoneRegexWithMinus("5555555555"));
+console.log(telephoneRegexWithMinus("1 555 555 5555"));
